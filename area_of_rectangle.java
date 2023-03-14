c9ed561c6ccb5962468a23e2d99bff22aba4c894
praneethsiriki <123744407+praneethsiriki@users.noreply.github.com>
2023-03-14 11:42:07 +0530

    Create area_of_rectangle.java

---
+++ area_of_rectangle.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+ public class area_of_rectangle{ 
+     public static void main(String args[]){ 
+         Scanner sc=new Scanner(System.in); 
+         System.out.println("enter breadth\n"); 
+         int b= sc.nextInt(); 
+         System.out.println("enter height\n"); 
+         int h= sc.nextInt(); 
+         int area=2*b*h; 
+         System.out.println("the area of rectangle is "+area); 
+     } 
+     }
